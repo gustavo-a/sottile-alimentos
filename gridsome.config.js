f508cc83cc7b4da37e6e6fbdecf9d2b438b947c9
Gustavo <gustavo@wyasi.com.br>
2019-12-04 17:12:36 -0300

    add sitemaps + robots.txt

--- gridsome.config.js
+++ gridsome.config.js
@@ -16,7 +16,15 @@ module.exports = {
   siteName: 'Sottile Alimentos',
   siteUrl: 'sotillealimentos.com.br',
   siteDescription: 'A gente acredita que é possível mudar o mundo, alimentar 7 bilhões de seres humanos de forma sustentável e, ao mesmo tempo, melhorar a saúde e a qualidade de vida das pessoas. Conheça o New Burger!',
-  plugins: [],
+  plugins: [
+    {
+      use: '@gridsome/plugin-sitemap',
+      options: {
+        cacheTime: 600000, // default
+        exclude: ['/obrigado']
+      }
+    }
+  ],
   css: {
     loaderOptions: {
       postcss: {
