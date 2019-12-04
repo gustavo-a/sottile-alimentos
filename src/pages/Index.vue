114383515c931766141133ba7f6a5842d67cc679
Gustavo <gustavo@wyasi.com.br>
2019-12-04 00:53:22 -0300

    form fix netlify

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -27,7 +27,7 @@
           <g-image src="../assets/images/contato.png" alt="Contato"></g-image>
           <h2 class="sr-only">Contato</h2>
        </div>
-        <form id="contact" class="w-full text-white" name="contact" action="/contato" method="POST" data-netlify="true" netlify-honeypot="bot-field">
+        <form id="contact" @submit.prevent="handleSubmit" class="w-full text-white" name="contact" action="/contato" method="POST" data-netlify="true" data-netlify-honeypot="bot-field">
           <div class="flex flex-wrap w-full">
             <div class="w-full sm:w-6/12 lg:w-5/12 lg:pr-4">
               <div class="hidden">
@@ -36,21 +36,21 @@
               </div>
 
               <label for="nome" class="sr-only">Nome</label>
-              <input placeholder="Nome" required name="nome" class="placeholder-white w-full sm:w-10/12 py-3 px-4 mb-5 bg-transparent border border-gray-300 rounded-lg" type="text" />
+              <input placeholder="Nome" required name="nome" class="placeholder-white w-full sm:w-10/12 py-3 px-4 mb-5 bg-transparent border border-gray-300 rounded-lg" type="text" v-model="formData.nome"/>
 
               <label for="empresa" class="sr-only">Empresa</label>
-              <input placeholder="Empresa" required class="placeholder-white w-full sm:w-10/12 py-3 px-4 mb-5 bg-transparent border border-gray-300 rounded-lg" name="empresa" type="text" />
+              <input placeholder="Empresa" required class="placeholder-white w-full sm:w-10/12 py-3 px-4 mb-5 bg-transparent border border-gray-300 rounded-lg" name="empresa" type="text" v-model="formData.empresa" />
 
               <label for="telefone" class="sr-only">Telefone</label>
-              <input placeholder="Telefone" required class="placeholder-white w-full sm:w-10/12 py-3 px-4 mb-5 bg-transparent border border-gray-300 rounded-lg" name="telefone" type="text" />
+              <input placeholder="Telefone" required class="placeholder-white w-full sm:w-10/12 py-3 px-4 mb-5 bg-transparent border border-gray-300 rounded-lg" name="telefone" type="text" v-model="formData.telefone"/>
 
               <label for="email" class="sr-only">E-mail</label>
-              <input placeholder="E-mail" required class="placeholder-white w-full sm:w-10/12 py-3 px-4 bg-transparent border border-gray-300 rounded-lg" name="email" type="mail" />
+              <input placeholder="E-mail" required class="placeholder-white w-full sm:w-10/12 py-3 px-4 bg-transparent border border-gray-300 rounded-lg" name="email" type="mail" v-model="formData.email"/>
 
             </div>
             <div class="w-full mt-8 sm:mt-0 sm:w-6/12 lg:w-5/12">
               <label for="mensagem" class="sr-only">E-mail</label>
-              <textarea form="contact" placeholder="Mensagem" required class="placeholder-white w-full sm:w-10/12 h-full py-3 px-4 bg-transparent border border-gray-300 rounded-lg" name="mensagem" id=""></textarea>
+              <textarea form="contact" placeholder="Mensagem" required class="placeholder-white w-full sm:w-10/12 h-full py-3 px-4 bg-transparent border border-gray-300 rounded-lg" name="mensagem" v-model="formData.mensagem"></textarea>
             </div>
             <div class="w-full mt-8 lg:w-2/12 lg-mt-0 self-end">
               <button class="bg-red-sottile border-3 border-red-800 py-3 px-5 rounded-full tracking-widest" type="submit"><strong>Enviar</strong></button>
@@ -85,6 +85,11 @@ export default {
   components:{
     SottileButton
   },
+  data(){
+    return {
+      formData: {}
+    }
+  },
   metaInfo: {
     title: 'Conheça o New Burger'
   },
@@ -112,6 +117,23 @@ export default {
         }
 
       })
+    },
+    encode(data) {
+      return Object.keys(data)
+        .map(key => encodeURIComponent(key) + '=' + encodeURIComponent(data[key]))
+        .join('&')
+    },
+    handleSubmit(e) {
+      fetch('/', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+        body: this.encode({
+          'form-name': e.target.getAttribute('name'),
+          ...this.formData,
+        }),
+      })
+      .then(() => this.$router.push('/contato'))
+      .catch(error => alert(error))
     }
   },
   mounted(){
