71b6bf4eb73335f25cc056c70744b81aaba629aa
Gustavo <gustavo@agenciaazul.com.br>
2020-02-03 15:12:28 -0300

    vue gtag

--- src/main.js
+++ src/main.js
@@ -2,19 +2,23 @@
 // The Client API can be used here. Learn more: gridsome.org/docs/client-api
 
 import DefaultLayout from '~/layouts/Default.vue'
+import VueGtag from 'vue-gtag'
 
 if (process.isClient) {
-  const WebFont = require('webfontloader');
+  const WebFont = require('webfontloader')
 
   WebFont.load({
     google: {
       families: ['Raleway:400,600,800&display=swap']
     }
-  });
+  })
 }
 
-export default function (Vue, { router, head, isClient }) {
+export default function(Vue, { router, head, isClient }) {
   // Set default layout as a global component
   Vue.component('Layout', DefaultLayout)
+  Vue.use(VueGtag, {
+    config: { id: 'UA-157606195-1' }
+  })
   head.htmlAttrs = { lang: 'pt-BR' }
 }
